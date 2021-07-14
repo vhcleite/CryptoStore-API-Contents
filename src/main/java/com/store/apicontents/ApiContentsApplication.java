7f1682dc145e620efaa4ce8fae4078a54724f0b8
Victor Leite <victor_leite@outlook.com.br>
2021-07-14 08:57:46 -0300

    Commit inicial

---
+++ src/main/java/com/store/apicontents/ApiContentsApplication.java
@@ -0,0 +1,13 @@
+package com.store.apicontents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/apicontents/ApiContentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.apicontents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiContentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
