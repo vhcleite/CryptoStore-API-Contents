24dde06f0d675ee3f2e228fe5dd5470b1ad2855e
Victor Leite <victor_leite@outlook.com.br>
2021-07-31 16:39:07 -0300

    Permitindo consumo da api de cross origin

--- src/main/java/com/store/apicontents/controller/ContentController.java
+++ src/main/java/com/store/apicontents/controller/ContentController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("store/v1/content")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ContentController {
 
     private ContentService contentService;
